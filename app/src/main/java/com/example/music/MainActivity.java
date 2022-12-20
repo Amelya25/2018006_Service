e71d7df86b8f65227266fe3eb69d5de99a214a3c
Amelya25 <guardian.gaia@gmail.com>
2022-12-20 23:38:03 +0700

    Tugas Service

---
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+   MediaPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View v) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.song);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+            Toast.makeText(this, "Lagu Lisa Unlasting dimainkan", Toast.LENGTH_SHORT).show();
+        }
+        player.start();
+
+    }
+
+    public void pause(View v) {
+        if (player != null) {
+            player.pause();
+            Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void stop(View v) {
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            Toast.makeText(this, "Musik Berhenti", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
+    
+}
